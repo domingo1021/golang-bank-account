ae1db4190d3e42f78b1e0dcb854eebaa8221fbd8
= <=>
2023-02-14 12:45:47 +0800

    [Feat] complete entry unit test

--- Docker/DB/sqlc/entry_test.go
+++ Docker/DB/sqlc/entry_test.go
@@ -37,9 +37,32 @@ func TestCreateEntry(t *testing.T) {
 }
 
 func TestGetEntry(t *testing.T) {
+	entry1 := CreateRandomEntry(t)
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
 
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.CreatedAt, entry2.CreatedAt)
+	require.Equal(t, entry1.Amount, entry2.Amount)
 }
 
 func TestListEntries(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		CreateRandomEntry(t)
+	}
 
+	args := ListEntriesParams{
+		Limit: 5,
+		Offset: 5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), args)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
 }
