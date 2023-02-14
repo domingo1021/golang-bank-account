b88e353da7ace8d16b66c4d3d9a29a25cc120832
= <=>
2023-02-14 13:27:32 +0800

    [Refactor] refoctor Random string with factory to create capital wise string

--- util/random.go
+++ util/random.go
@@ -25,13 +25,26 @@ func RandomInt(min, max int64) int64 {
 	return min + rand.Int63n(max - min + 1)
 }
 
+func RandomUpperCase() byte {
+	return byte(RandomInt(65, 90))
+}
+
+func RandomLowerCase() byte {
+	return byte(RandomInt(97, 122))
+}
+
+var capitalWiseFactory = map[int64]func()(byte){
+	0: RandomLowerCase,
+	1: RandomUpperCase,
+}
+
 // RandomString generate a random string of length n
 func RandomString(n int) string {
 	var sb strings.Builder
-	k := len(alphabet)
 	
 	for i := 0; i < n; i++ {
-		c := alphabet[rand.Intn(k)]
+		capitalWise := RandomInt(0, 1)
+		c := capitalWiseFactory[capitalWise]()
 		sb.WriteByte(c)
 	}
 	return sb.String();
