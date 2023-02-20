718911c4e40865d0c4efab94063c022bec1e04b9
= <=>
2023-02-20 17:51:06 +0800

    [Feat] add list accounts api

--- api/account.go
+++ api/account.go
@@ -17,6 +17,11 @@ type getAccountRequest struct {
 	ID int64 `uri:"id" binding:"required,min=1"`
 }
 
+type listAccountRequest struct {
+	pageID   int32 `form:"page_id" binding:"required,min=1"`
+	pageSize int32 `form:"page_size" binding:"required,min=5, max=10"`
+}
+
 func (server *Server) createAccount(ctx *gin.Context) {
 	var req createAccountRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
@@ -56,4 +61,24 @@ func (server *Server) getAccount(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(http.StatusOK, account)
+}
+
+func (server *Server) listAccounts(ctx *gin.Context) {
+	var req listAccountRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	args := db.ListAccountsParams{
+		Limit:  req.pageID,
+		Offset: (req.pageID - 1) * req.pageSize,
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, args)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, accounts)
 }
