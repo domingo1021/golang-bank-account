f6a657317c0f3cafa615d99822eab3abbd81af37
= <=>
2023-02-20 21:47:58 +0800

    [Feat] add env setting for project

--- main.go
+++ main.go
@@ -6,18 +6,17 @@ import (
 
 	"github.com/domingo1021/golang-bank-account/api"
 	db "github.com/domingo1021/golang-bank-account/db/sqlc"
+	"github.com/domingo1021/golang-bank-account/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:password1234@localhost:5432/bank_account?sslmode=disable"
-	serverAddress = ":8080"
-)
-
 func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot read env variable: ", err)
+	}
 
-	conn, err := sql.Open(dbDriver, dbSource)
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
@@ -25,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("start web server failed: ", err)
 	}
